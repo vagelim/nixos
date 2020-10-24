9b4b1325706cf0b5d2f5d88aa677a61284c372e5
vagelim <vageli.mouzakitis@candidco.com>
2020-10-24 18:18:41 -0400

    init

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo systemctl start sshd
+sudo parted /dev/sda -- mklabel msdos
+sudo parted /dev/sda -- mkpart primary 1MiB -2GiB
+sudo parted /dev/sda -- mkpart primary linux-swap -2GiB 100%
+sudo mkfs.ext4 -L nixos /dev/sda1
+sudo mkswap -L swap /dev/sda2
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo nixos-generate-config --root /mnt
+sudo nano /mnt/etc/nixos/configuration.nix
