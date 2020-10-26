57a0335bedbf61508c8abc5cf6cf567befa20e77
vagelim <vageli.mouzakitis@candidco.com>
2020-10-26 00:24:01 -0400

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -30,31 +30,11 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     ag chromium firefox gitAndTools.gitFull htop qemu-utils wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  #   pinentryFlavor = "gnome3";
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
- networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -75,11 +55,6 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.desktopManager.xterm.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
 
   users.users.test = {
     isNormalUser = true;
@@ -87,9 +62,6 @@
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMOlEzksQh2pKQ1V6daqoKlcUdCRSTlab9nk9TPzR9dLiC+zsJyJg8nWEu1q+1+QfPtco2+mswoJujnSl7hgQIb+7QmeoqEZXTG2YtixOtucdIoRc6rW0713JVQcT6zET8juj2ydyswGad4buMP3oYAX5pQswyfLVtugokw5szdklOPKEWgsKqPvWeeawV4fi7xZ3WYpP/sJt+Gv6X4R6k5RvaSm4l4bH9FnZS2VN2Qg+obO+XDa+tukuL6lvVtvY9c3aVh6xR9El54SYNmEeOPaWS/KIGAniRGXzXmgUXshZGZ+LxtJIBTpHmSyaPglJzrPNJqDtkBNhSVTrBTihsfJuLp6i+rihAJ8YFNT5zEH19SaSJeI+vATImSprMe3qkHVIDRRb4bCrOFxHgrj7GPWSYkPphZTi5hOCIimuEY1IRZaaDVflfBsw4xC/gkZLenZ2sw2sKoSy4QwuE+P0pOLwfAchvtpFt/tg/Zp8t1EP+Qo6M9yY16j20aptWe20= kr" ];
   };
 
-  # qemu guest for proxmox
-  services.qemuGuest.enable = true;
-  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -98,4 +70,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.03"; # Did you read the comment?
   
+  environment.systemPackages = with pkgs; [
+     ag chromium firefox gitAndTools.gitFull htop qemu-utils screen wget
+  ];
 }
